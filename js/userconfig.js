b0e42882380b08ae965ce054484b50ce86c1c187
rheywood <becca.heywood@gmail.com>
2016-10-27 15:13:44 -0400

    Update userconfig.js

--- js/userconfig.js
+++ js/userconfig.js
@@ -4,15 +4,12 @@
 
 var config = {
 	//Set Map Bounds & point map is centered around
-	mapFocus : [40.730610, -73.935242],
-	south : 40.59 ,
-	west : -74.08,
-	north : 40.80,
-	east : -73.75,
+	center : [40.730610, -73.935242],
+	zoom: 9
 	//Mapbox access token & key for basemap
 	mapboxAccessToken :              
             'pk.eyJ1IjoicmhleXdvb2QiLCJhIjoiY2l1cmZnYXJlMDA4MDJ5cnY2azJvNmNtNCJ9.HqJZu0f7QVenQA2vtk2z5w',
-	basemap: 'https://api.mapbox.com/styles/v1/rheywood/ciuskj4zn00hi2io2hkcxkjni/static/-73.953331,40.729284,9.81,0.00,0.00/800x800?access_token=pk.eyJ1IjoicmhleXdvb2QiLCJhIjoiY2l1cmZnYXJlMDA4MDJ5cnY2azJvNmNtNCJ9.HqJZu0f7QVenQA2vtk2z5w',
+	style: 'mapbox://styles/rheywood/ciuskj4zn00hi2io2hkcxkjni',
     //Change to your username, insert function on cartodb, and cartodb tablename (see also /cartoDB_functions)
 	cartoDBusername : 'rheywood',
 	cartoDBinsertfunction : 'insert_transitways_data',
