c4a91b8d589c8bacaff9327252157adac5eb530e
rheywood <becca.heywood@gmail.com>
2016-11-17 10:25:27 -0500

    Update userconfig.js

--- js/userconfig.js
+++ js/userconfig.js
@@ -1,19 +1,21 @@
+
+
 /*
  * Central repository of options to change when forking this map!
  */
 
- <!-- set up the map -->
-      <div id='map'></div>
-        <script>
-        L.mapbox.accessToken = 'pk.eyJ1IjoicmhleXdvb2QiLCJhIjoiY2l1cmZnYXJlMDA4MDJ5cnY2azJvNmNtNCJ9.HqJZu0f7QVenQA2vtk2z5w';
-        var map = L.mapbox.map('map', 'rheywood.ciuskj4zn00hi2io2hkcxkjni', {zoomControl: false}).setView([40.730, -73.939], 14); 
-       // map.doubleClickZoom.disable();
-        new L.Control.Zoom({ position: 'bottomright' }).addTo(map);
-    
-
+var config = {
+	//Set Map Bounds & point map is centered around
+	mapFocus : [40.730, -73.939],
+	south : 40.24 ,
+	west : -74.27,
+	north : 40.953,
+	east : -72.95,
+	//Mapbox access token & key for basemap
+	mapboxAccessToken :              
+            'pk.eyJ1IjoicmhleXdvb2QiLCJhIjoiY2l1cmZnYXJlMDA4MDJ5cnY2azJvNmNtNCJ9.HqJZu0f7QVenQA2vtk2z5w',
     //Change to your username, insert function on cartodb, and cartodb tablename (see also /cartoDB_functions)
 	cartoDBusername : 'rheywood',
 	cartoDBinsertfunction : 'insert_transitways_data',
 	walkthroughWelcome: "<p>This webmap allows you to view data collected by the New York City Department of Transportation on public transportation in NYC.</p><p>It also allows you to submit input on where transit could be improved by drawing on the map! </p>"
-    </script>
-
+};
